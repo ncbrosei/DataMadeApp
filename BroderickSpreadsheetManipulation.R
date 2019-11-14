a128680b4cbfe350bf6cfced8b08b6c50c0d7481
ncbrosei <ncbroderick12@gmail.com>
2019-11-14 12:53:04 -0600

    Update BroderickSpreadsheetManipulation.R

--- BroderickSpreadsheetManipulation.R
+++ BroderickSpreadsheetManipulation.R
@@ -6,6 +6,8 @@ legislators <- read.csv(file = "legislators.csv")
 
 #split Rs and Ds into separate sheets
 #create new dataframes based on the value in the "party" column (D/R/I)
+#Indies <- subset(legislators, legislators$party == "I")
+#3 Independents, 450 democrats, 500 republicans. No data seems to be missing.
 repubs <- subset(legislators, legislators$party == "R")
 dems <- subset(legislators, legislators$party == "D")
 
@@ -15,8 +17,12 @@ dems <- subset(legislators, legislators$party == "D")
 #find todays date
 today <- Sys.Date()
 
-#calculate age: 45 years * 365 + 11 for leap years = 16436 days
 #convert the value in birthdate into R date structure and test against todays date
+#test <- dems[c(5), ]
+#bDayTest <- as.Date(test$birthdate)
+#today - bDayTest
+
+#calculate age: 45 years * 365 + 11 for leap years = 16436 days
 #Subset any values that are younger than 45 (16436 days)
 demsLegs <- subset(dems, (today- as.Date(dems$birthdate)) < 16436)
 
